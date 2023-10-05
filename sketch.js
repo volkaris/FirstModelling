6d8abfc5f82e9070ec174d72aa7d50aa257faa00
Ilya <110740644+volkaris@users.noreply.github.com>
2023-10-05 12:30:41 +0300

    /

--- sketch.js
+++ sketch.js
@@ -1,13 +1,18 @@
 
 let countDiv;
 let count = 0;
+let first_body;
+let second_body;
+let digits=5;
+let FPS=10**digits-2
 class body {
-	constructor(x_coordinate,width,velocity,mass) {
+	constructor(x_coordinate,width,velocity,mass,constrain) {
 		this.x=x_coordinate;
 		this.y=height-width;
 		this.w=width;
 		this.v=velocity;
-		this.m=mass
+		this.m=mass;
+		this.constrain=constrain  //дальше этой координаты блок не может рисоваться
 	}
 
 	update(){
@@ -15,7 +20,8 @@ class body {
 	}
 
 	show(){
-		square(this.x, this.y,this.w);
+		const x = constrain(this.x, this.constrain, width);
+		square(x, this.y,this.w);
 
 	}
 
@@ -25,10 +31,7 @@ class body {
 
 	}
 	push_off(other){
-		let sumM = this.m + other.m;
-		let newV = (this.m - other.m) / sumM * this.v;
-		newV += (2 * other.m / sumM) * other.v;
-		return newV;
+		return ((this.m-other.m)/(this.m+other.m))*this.v+ ((2*other.m)/(this.m+other.m))*other.v
 	}
 
 	hit_the_wall (){
@@ -41,8 +44,7 @@ class body {
 
 }
 
-let first_body;
-let second_body;
+
 
 
 function preload() {
@@ -51,38 +53,43 @@ function preload() {
 
 function setup() {
 	createCanvas(3000, 300);
-
 	background(51);
+
 	countDiv = createDiv(count);
-	first_body=new body(100,20,0,1000);
-	second_body=new body(200,150,-90,10000000)
+
+	let m2=100**digits-1
+	first_body=new body(100,20,0,1,0);
+	second_body=new body(200,150,-1/FPS,m2,first_body.w)    //-1/FPS,m2 so same velocity but divided by amount of frames per second
 
 }
 
 function draw() {
 	background(0);
-	if (first_body.bump(second_body)){
-		const v1 = first_body.push_off(second_body);
-		const v2 = second_body.push_off(first_body);
-		first_body.v = v1;
-		second_body.v = v2;
-		count++;
 
-	}
-	if(first_body.hit_the_wall()){
+	for (let i=0;i<FPS;++i) {
+
+		if (first_body.bump(second_body)) {
+			let v1 = first_body.push_off(second_body);
+			let v2 = second_body.push_off(first_body);
+			first_body.v = v1;
+			second_body.v = v2;
+			count++;
+
+		}
+		if (first_body.hit_the_wall()) {
 			first_body.reverse()
-		count++;
+			count++;
+		}
+
+
+		first_body.update();
+		second_body.update();
 	}
-
-
-	first_body.update();
-	second_body.update();
-
 	first_body.show();
 	second_body.show();
 
 
-countDiv.html(count)
+countDiv.html(nf(count,digits+1))
 
 
 }
