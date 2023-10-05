67ca0fe6aa773554c8fee9e5019607ba260c4160
Ilya <110740644+volkaris@users.noreply.github.com>
2023-10-05 21:17:40 +0300

    /

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
-
-let countDiv;
 let count = 0;
+
 let first_body;
 let second_body;
+
 let digits=5;
+
 let FPS=10**digits-2
 class body {
 	constructor(x_coordinate,width,velocity,mass,constrain) {
@@ -14,21 +15,16 @@ class body {
 		this.m=mass;
 		this.constrain=constrain  //дальше этой координаты блок не может рисоваться
 	}
-
 	update(){
 		this.x+=this.v;
 	}
-
 	show(){
-		const x = constrain(this.x, this.constrain, width);
-		square(x, this.y,this.w);
-
+		square(constrain(this.x, this.constrain, width), this.y,this.w);
 	}
 
 	bump(other) {
 		return !(this.x + this.w < other.x ||
 			this.x > other.x + other.w); // +this.w so we check not center of body but left/right corner
-
 	}
 	push_off(other){
 		return ((this.m-other.m)/(this.m+other.m))*this.v+ ((2*other.m)/(this.m+other.m))*other.v
@@ -40,56 +36,37 @@ class body {
 	reverse() {
 		this.v *= -1;
 	}
-
-
 }
-
-
-
-
-function preload() {
-	blockImg = loadImage('block.png');
-}
-
 function setup() {
 	createCanvas(3000, 300);
 	background(51);
 
-	countDiv = createDiv(count);
-
 	let m2=100**digits-1
+
 	first_body=new body(100,20,0,1,0);
 	second_body=new body(200,150,-1/FPS,m2,first_body.w)    //-1/FPS,m2 so same velocity but divided by amount of frames per second
-
 }
-
 function draw() {
 	background(0);
-
 	for (let i=0;i<FPS;++i) {
-
 		if (first_body.bump(second_body)) {
 			let v1 = first_body.push_off(second_body);
 			let v2 = second_body.push_off(first_body);
 			first_body.v = v1;
 			second_body.v = v2;
 			count++;
-
 		}
+
 		if (first_body.hit_the_wall()) {
 			first_body.reverse()
 			count++;
 		}
-
-
 		first_body.update();
 		second_body.update();
 	}
+
+	document.getElementById("counter").textContent = "Amount of collisions: " + count
+
 	first_body.show();
 	second_body.show();
-
-
-countDiv.html(nf(count,digits+1))
-
-
 }
